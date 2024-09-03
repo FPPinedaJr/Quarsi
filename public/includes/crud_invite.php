442eb3da388793658af59406d24604ec8ab7f782
23jammy <23jammycruz@gmail.com>
2024-09-03 14:33:49 +0800

    Null logtime (#41)
    * Initial layout for invited students
    
    * Modified crud_invite
    
    * Initial layout for null attendance; query structure must be fixed
    
    * Modified way of accessing id from invite students
    
    * Modifid add invite
    
    * Resolved conflicts; added cancel button to end event

--- public/includes/crud_invite.php
+++ public/includes/crud_invite.php
@@ -2,66 +2,73 @@
 session_start();
 include_once "../includes/connect_db.php";
 
-if  ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "<pre>";
     print_r($_POST);
     echo "</pre>";
     $students = $_POST['students'];
-    $idevent = $_POST['idevent']; 
-    $def_time = "00:00:00";
+    $logtime = $_POST['logtime'];
+    $idevent = $_POST['idevent'];
+    $null = NULL;
+    $def = "00:00:00";
 
-    $stmt = $pdo->prepare("SELECT log_time FROM event WHERE idevent=:idevent"); 
-    $stmt->bindParam(':idevent', $idevent, PDO::PARAM_INT); 
-    $stmt->execute();
-    $log_time = $stmt->fetchColumn();
+    $morning_in = NULL;
+    $morning_out = NULL;
+    $afternoon_in = NULL;
+    $afternoon_out = NULL;
 
-    foreach ($students as $student_id) {
-        if ($log_time == 1) {
-            $stmt1 = $pdo->prepare( "
-            INSERT INTO attendance (event, user, morning_in) 
-            VALUES (:event, :user, :morning_in)
-            ");
-            $stmt1->bindParam(':event', $idevent, PDO::PARAM_INT);
-            $stmt1->bindParam(':user', $student_id, PDO::PARAM_INT);
-            $stmt1->bindParam(':morning_in', $def_time, PDO::PARAM_STR);
+    if (in_array(1, $logtime)) {
+        $morning_in = $def;
+    }
+    if (in_array(2, $logtime)) {
+        $morning_out = $def;
+    }
+    if (in_array(3, $logtime)) {
+        $afternoon_in = $def;
+    }
+    if (in_array(4, $logtime)) {
+        $afternoon_out = $def;
+    }
 
-        } elseif ($log_time == 2) {
-            $stmt1 = $pdo->prepare( "
-            INSERT INTO attendance (event, user, morning_out) 
-            VALUES (:event, :user, :morning_out)
-            ");
-            $stmt1->bindParam(':event', $idevent, PDO::PARAM_INT);
-            $stmt1->bindParam(':user', $student_id, PDO::PARAM_INT);
-            $stmt1->bindParam(':morning_out', $def_time, PDO::PARAM_STR);
+    foreach ($students as $student_id): 
+        $stmt1 = $pdo->prepare("
+                INSERT INTO attendance (event, user, morning_in, morning_out, afternoon_in, afternoon_out) 
+                VALUES (:event, :user, :morning_in, :morning_out, :afternoon_in, :afternoon_out)
+                ");
+        $stmt1->bindParam(':event', $idevent, PDO::PARAM_INT);
+        $stmt1->bindParam(':user', $student_id, PDO::PARAM_INT);
 
-        } elseif ($log_time == 3) {
-            $stmt1 = $pdo->prepare( "
-            INSERT INTO attendance (event, user, afternoon_in) 
-            VALUES (:event, :user, :afternoon_in)
-            ");
-            $stmt1->bindParam(':event', $idevent, PDO::PARAM_INT);
-            $stmt1->bindParam(':user', $student_id, PDO::PARAM_INT);
-            $stmt1->bindParam(':afternoon_in', $def_time, PDO::PARAM_STR);
-
-        } elseif ($log_time == 4) {
-            $stmt1 = $pdo->prepare( "
-            INSERT INTO attendance (event, user, afternoon_out) 
-            VALUES (:event, :user, :afternoon_out)
-            ");
-            $stmt1->bindParam(':event', $idevent, PDO::PARAM_INT);
-            $stmt1->bindParam(':user', $student_id, PDO::PARAM_INT);
-            $stmt1->bindParam(':afternoon_out', $def_time, PDO::PARAM_STR);
+        if (is_null($morning_in)) {
+            $stmt1->bindParam(':morning_in', $morning_in, PDO::PARAM_NULL);
+        } else {
+            $stmt1->bindParam(':morning_in', $morning_in, PDO::PARAM_STR);
         }
 
+        if (is_null($morning_out)) {
+            $stmt1->bindParam(':morning_out', $morning_out, PDO::PARAM_NULL);
+        } else {
+            $stmt1->bindParam(':morning_out', $morning_out, PDO::PARAM_STR);
+        }
+
+        if (is_null($afternoon_in)) {
+            $stmt1->bindParam(':afternoon_in', $afternoon_in, PDO::PARAM_NULL);
+        } else {
+            $stmt1->bindParam(':afternoon_in', $afternoon_in, PDO::PARAM_STR);
+        }
+
+        if (is_null($afternoon_out)) {
+            $stmt1->bindParam(':afternoon_out', $afternoon_out, PDO::PARAM_NULL);
+        } else {
+            $stmt1->bindParam(':afternoon_out', $afternoon_out, PDO::PARAM_STR);
+        }
 
         if ($stmt1->execute()) {
             echo "Recorded students";
         } else {
             echo "Error inserting attendance for student ID $student_id. Please try again.";
         }
-    }
-
+    endforeach;
+    
     header("Location: ../events.php");
-    exit(); 
+    exit();
 }
-
