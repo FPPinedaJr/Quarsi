197109071bfbb3e8a0cbb805ea5e299c48a0a7c3
fppinedajr <fernandojr03.pineda@gmail.com>
2024-09-03 16:43:30 +0800

    Error Handling

--- public/includes/crud_admin.php
+++ public/includes/crud_admin.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $total_points = $_POST['total_points'];
             $profile_pic = $_FILES['profile_pic'];
             $user = 1;
-            $img_content = ""; 
+            $img_content = "";
 
             if (!empty($profile_pic["tmp_name"])) {
                 $source = $profile_pic["tmp_name"];
@@ -40,12 +40,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 imagecopyresampled($tn, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
                 ob_start();
-                imagejpeg($tn, NULL, 60); 
+                imagejpeg($tn, NULL, 60);
                 $img_content = ob_get_clean();
-                } else {
-                    $img_content = file_get_contents("../assets/images/default_pic.jpg");
-                }
-            
+            } else {
+                $img_content = file_get_contents("../assets/images/default_pic.jpg");
+            }
+
             if ($user_type == 0) {
                 $stmt = $pdo->prepare("
                     UPDATE user
@@ -64,24 +64,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
                 $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
 
-            } else if ($user_type == 1){
+            } else if ($user_type == 1) {
                 $stmt = $pdo->prepare("
                     UPDATE user
                     SET student_no=:student_no, f_name=:f_name, l_name=:l_name, organization=:organization,
                         year=:year, block=:block, email=:email, is_officer=:user, total_points=:total_points, profile_pic=:profile_pic
                     WHERE iduser=:iduser
                     ");
-                    $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
-                    $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
-                    $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
-                    $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
-                    $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
-                    $stmt->bindParam(':year', $year, PDO::PARAM_INT);
-                    $stmt->bindParam(':block', $block, PDO::PARAM_INT);
-                    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-                    $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
-                    $stmt->bindParam(':user', $user, PDO::PARAM_INT);
-                    $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
+                $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
+                $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
+                $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
+                $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
+                $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
+                $stmt->bindParam(':year', $year, PDO::PARAM_INT);
+                $stmt->bindParam(':block', $block, PDO::PARAM_INT);
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
+                $stmt->bindParam(':user', $user, PDO::PARAM_INT);
+                $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
 
             } else if ($user_type == 2) {
                 $stmt = $pdo->prepare("
@@ -90,17 +90,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         year=:year, block=:block, email=:email, is_superuser=:user, total_points=:total_points, profile_pic=:profile_pic
                     WHERE iduser=:iduser
                     ");
-                    $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
-                    $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
-                    $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
-                    $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
-                    $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
-                    $stmt->bindParam(':year', $year, PDO::PARAM_INT);
-                    $stmt->bindParam(':block', $block, PDO::PARAM_INT);
-                    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-                    $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
-                    $stmt->bindParam(':user', $user, PDO::PARAM_INT);
-                    $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
+                $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
+                $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
+                $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
+                $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
+                $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
+                $stmt->bindParam(':year', $year, PDO::PARAM_INT);
+                $stmt->bindParam(':block', $block, PDO::PARAM_INT);
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
+                $stmt->bindParam(':user', $user, PDO::PARAM_INT);
+                $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
 
             } else if ($user_type == 3) {
                 $stmt = $pdo->prepare("
@@ -109,21 +109,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         year=:year, block=:block, email=:email, is_admin=:user, total_points=:total_points, profile_pic=:profile_pic
                     WHERE iduser=:iduser
                     ");
-                    $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
-                    $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
-                    $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
-                    $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
-                    $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
-                    $stmt->bindParam(':year', $year, PDO::PARAM_INT);
-                    $stmt->bindParam(':block', $block, PDO::PARAM_INT);
-                    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-                    $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
-                    $stmt->bindParam(':user', $user, PDO::PARAM_INT);
-                    $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
+                $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
+                $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
+                $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
+                $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
+                $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
+                $stmt->bindParam(':year', $year, PDO::PARAM_INT);
+                $stmt->bindParam(':block', $block, PDO::PARAM_INT);
+                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                $stmt->bindParam(':total_points', $total_points, PDO::PARAM_INT);
+                $stmt->bindParam(':user', $user, PDO::PARAM_INT);
+                $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
             }
             if ($stmt->execute()) {
                 header("Location: ../admin.php");
-                exit(); 
+                exit();
             } else {
                 echo "Error updating user. Please try again.";
             }
@@ -138,7 +138,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $stmt->bindParam(':iduser', $iduser, PDO::PARAM_INT);
             if ($stmt->execute()) {
                 header("Location: ../admin.php");
-                exit(); 
+                exit();
             } else {
                 echo "Error deleting user. Please try again.";
             }
@@ -153,7 +153,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $password = $_POST['student_no'];
             $profile_pic = $_FILES['profile_pic'];
             $user = 1;
-            $img_content = ""; 
+            $img_content = "";
 
             if (!empty($profile_pic["tmp_name"])) {
                 $source = $profile_pic["tmp_name"];
@@ -176,32 +176,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 imagecopyresampled($tn, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 
                 ob_start();
-                imagejpeg($tn, NULL, 60); 
+                imagejpeg($tn, NULL, 60);
                 $img_content = ob_get_clean();
-                } else {
-                    $img_content = file_get_contents("../assets/images/default_pic.jpg");
-                }            
+            } else {
+                $img_content = file_get_contents("../assets/images/default_pic.jpg");
+            }
+
+            $stmt = $pdo->prepare("
+                SELECT * FROM user 
+                WHERE student_no=:student_no
+            ");
+            $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
+
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row) {
+                header("location: ../error_user.php");
+                exit();
+            }
+
             $stmt = $pdo->prepare("
                 INSERT INTO user (student_no, f_name, l_name, organization, year, block, email, password, is_admin, profile_pic)
                 VALUES (:student_no, :f_name, :l_name, :organization, :year, :block, :email, SHA2(:password, 256), :user, :profile_pic)");
 
-                $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
-                $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
-                $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
-                $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
-                $stmt->bindParam(':year', $year, PDO::PARAM_INT);
-                $stmt->bindParam(':block', $block, PDO::PARAM_INT);
-                $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-                $stmt->bindParam(':password', $password, PDO::PARAM_STR);
-                $stmt->bindParam(':user', $user, PDO::PARAM_INT);
-                $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
-            }
+            $stmt->bindParam(':student_no', $student_no, PDO::PARAM_STR);
+            $stmt->bindParam(':f_name', $f_name, PDO::PARAM_STR);
+            $stmt->bindParam(':l_name', $l_name, PDO::PARAM_STR);
+            $stmt->bindParam(':organization', $organization, PDO::PARAM_INT);
+            $stmt->bindParam(':year', $year, PDO::PARAM_INT);
+            $stmt->bindParam(':block', $block, PDO::PARAM_INT);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+            $stmt->bindParam(':user', $user, PDO::PARAM_INT);
+            $stmt->bindParam(':profile_pic', $img_content, PDO::PARAM_LOB);
+        }
 
-            if ($stmt->execute()) {
-                header("Location: ../admin.php");
-                exit(); 
-            } else {
-                echo "Error adding user. Please try again.";
-            }
+        if ($stmt->execute()) {
+            header("Location: ../admin.php");
+            exit();
+        } else {
+            echo "Error adding user. Please try again.";
+        }
     }
 }
