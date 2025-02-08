789cc3cc4479755469e0ce3c640846658b181aa7
23jammy <23jammycruz@gmail.com>
2025-02-08 19:50:00 +0800

    Modify students order depending in user type

--- public/student.php
+++ public/student.php
@@ -27,7 +27,7 @@ if (!$_SESSION["logged_in"] || !($_SESSION['is_officer'] == 1 || $_SESSION['is_s
       INNER JOIN organization
       ON user.organization = organization.idorganization
       WHERE user.is_admin <> 1
-      ORDER BY user.year, user.block, user.f_name;
+      ORDER BY is_superuser DESC, is_officer DESC, user.year, user.block, user.f_name;
     ");
 
     $stmt1->execute();
@@ -193,7 +193,7 @@ if (!$_SESSION["logged_in"] || !($_SESSION['is_officer'] == 1 || $_SESSION['is_s
                                 } ?>" class="cursor-pointer hover:bg-gray-300 even:bg-[#EDF4F2] odd:bg-gray-200" onclick="showEditStudentModal(<?=$student['iduser']?>)">
                                     <td class="py-2 pl-3 flex items-center"><img src="<?php if ($student['profile_pic']) {echo 'data:image/jpeg;base64,'. base64_encode($student['profile_pic']);}?>"
                                     class="rounded-full mr-2 border border-gray-400 w-6 h-6" loading="lazy"> 
-                                    <?=$student['f_name']?> <?=$student['l_name']?></td>
+                                    <p class="<?php if($student['is_superuser'] == 0 && $student['is_officer'] == 1) {echo 'text-blue-600';} else if ($student['is_superuser'] == 1 && $student['is_officer'] == 1) {echo 'text-violet-500';}?> font-semibold"><?=$student['f_name']?> <?=$student['l_name']?></p></td>
                                     <td class="py-2 pl-3"><?=$student['student_no']?></td>
                                     <td class="py-2 pl-3"><?=$student['program']?> <?=$student['year']?> Block <?=$student['block']?></td>
                                 </tr>
@@ -205,6 +205,7 @@ if (!$_SESSION["logged_in"] || !($_SESSION['is_officer'] == 1 || $_SESSION['is_s
             <!-- Mobile View -->
             <div class="flex flex-col w-full items-center md:hidden mt-10">
                 <?php foreach ($students as $student): ?>
+                    <?php if ($student['is_superuser'] == 1)?>
                     <div id="student-<?php echo $student['iduser'] ?>"
                         data-student_no="<?php echo $student['student_no'] ?>" data-f_name="<?php echo $student['f_name'] ?>"
                         data-l_name="<?php echo $student['l_name'] ?>" data-idprogram="<?php echo $student['idprogram_user'] ?>"
@@ -224,7 +225,7 @@ if (!$_SESSION["logged_in"] || !($_SESSION['is_officer'] == 1 || $_SESSION['is_s
                             <img src="data:image/jpeg;base64,<?= base64_encode($student['profile_pic']) ?>" alt="profile picture" class="w-16 h-16 rounded-full border border-gray-200">
                         </div>
                         <div class="w-2/3 h-full pl-2 p-1 flex justify-center flex-col text-nowrap">
-                            <p class="font-semibold text-xl"><?php echo $student['f_name'] ?> <?php echo $student['l_name'] ?></p>
+                            <p class="font-semibold text-xl <?php if($student['is_superuser'] == 0 && $student['is_officer'] == 1) {echo 'text-blue-600';} else if ($student['is_superuser'] == 1 && $student['is_officer'] == 1) {echo 'text-violet-500';}?>"><?php echo $student['f_name'] ?> <?php echo $student['l_name'] ?></p>
                             <p class="text-gray-700"><?php echo $student['student_no'] ?></p>
                             <p class=""><?php echo $student['program'] ?> <?php echo $student['year'] ?> Block <?php echo $student['year'] ?></p>
                         </div>
