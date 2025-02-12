5f07e748b0e7d6041601eaaddb698587dcfa992e
fppinedajr <fernandojr03.pineda@gmail.com>
2025-02-12 20:39:44 +0800

    Change database to be production

--- public/includes/connect_db.php
+++ public/includes/connect_db.php
@@ -1,29 +1,52 @@
 <?php
+function loadEnv($path)
+{
+	if (!file_exists($path)) {
+		throw new Exception("Environment file not found.");
+	}
+
+	$lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	foreach ($lines as $line) {
+		if (strpos(trim($line), '#') === 0) {
+			continue;
+		}
+
+		list($key, $value) = explode('=', $line, 2);
+		putenv("$key=$value");
+	}
+}
+
+
+// Load the `.env` file
+loadEnv('/home/u273960544/domains/miceff.com/quarsi.env'); 
+
 date_default_timezone_set("Asia/Manila");
 $charset = 'utf8mb4';
 
-// ----- PRODUCTION DATABASE ----- //
-$hostname = '77.37.35.51';
-$port = 3306 ;
-$username = 'u273960544_fpjcm_quarsi';
-$password = '12345siX*';
-$defaultSchema = 'u273960544_quarsi';
+// ----- PRODUCTION  DATABASE ----- //
+$hostname = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$defaultSchema = getenv('DB_NAME');
+$charset = getenv('DB_CHARSET');
+
 
 // ----- DEVELOPMENT DATABASE ----- //
-// $hostname = '77.37.35.51';
-// $port = 3306 ;
-// $username = 'u273960544_tester';
-// $password = 'TesTing123*';
-// $defaultSchema = 'u273960544_prototype';
-
-
+// $hostname      =  ; 
+// $port          =  ;
+// $username      =  ;
+// $password      =  ;
+// $defaultSchema =  ;
 
 $dsn = "mysql:host=$hostname;dbname=$defaultSchema;charset=$charset;port=$port";
 
-$option =[PDO::ATTR_ERRMODE 			=> PDO::ERRMODE_EXCEPTION,
-		 PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,
-		 PDO::ATTR_EMULATE_PREPARES		=> false];
+$option = [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES => false
+];
 
 global $pdo;
-$pdo= new PDO($dsn,$username,$password,$option);
+$pdo = new PDO($dsn, $username, $password, $option);
 
